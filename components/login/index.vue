191b81a42a3a1789ae1dccc64da71b7c7a3b665c
preciouStar <seongjin605@users.noreply.github.com>
2020-03-05 00:20:00 +0900

    Create Nuxt.js Project

---
+++ components/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>{{ say }}</h1>
+</template>
+<script>
+import { getHi } from '~/assets/test';
+export default {
+    data() {
+        return {
+            say: null
+        };
+    },
+    created() {
+        this.say = getHi();
+    }
+};
+</script>
